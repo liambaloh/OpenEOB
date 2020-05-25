51b8fe041bb6080280fadfd3b6378c85d6249ad1
LiamLime <contact@liamlime.com>
2020-05-25 05:18:35 +0100

    - Adds on-map animations - Adds more doors

---
+++ Assets/OpenEoB/Views/AnimationViews/AnimationView.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenEoB.Views.AnimationViews
+{
+    public class AnimationView : MonoBehaviour
+    {
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private List<Texture> _textures;
+        [SerializeField] private float _timeBetweenSprites;
+
+        private int _currentSpriteIndex;
+        [SerializeField] private bool _animationPlaying;
+        private float _animationTimer;
+
+        private void Start()
+        {
+            ResetAnimation();
+        }
+        
+        private void Update()
+        {
+            if (_animationPlaying)
+            {
+                _animationTimer += Time.deltaTime;
+                if (_animationTimer >= _timeBetweenSprites)
+                {
+                    _animationTimer -= _timeBetweenSprites;
+                    if (_currentSpriteIndex + 1 >= _textures.Count)
+                    {
+                        _animationPlaying = false;
+                        return;
+                    }
+
+                    _currentSpriteIndex++;
+                    _renderer.material.mainTexture = _textures[_currentSpriteIndex];
+                }
+            }
+        }
+
+        public void ResetAnimation()
+        {
+            _animationPlaying = false;
+            _currentSpriteIndex = 0;
+            _renderer.material.mainTexture = _textures[_currentSpriteIndex];
+        }
+
+        public void PlayAnimation()
+        {
+            _animationPlaying = true;
+            _animationTimer = 0;
+        }
+    }
+}
---
+++ Assets/OpenEoB/Views/ClickableObjectViews/PlayAnimationClickableObject.cs
@@ -0,0 +1,15 @@
+﻿using OpenEoB.Views.AnimationViews;
+using UnityEngine;
+
+namespace OpenEoB.Views.ClickableObjectViews
+{
+    public class PlayAnimationClickableObject : AbstractClickableObjectView
+    {
+        [SerializeField] private AnimationView _animationView;
+
+        protected override void Clicked()
+        {
+            _animationView.PlayAnimation();
+        }
+    }
+}
